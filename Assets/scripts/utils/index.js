025d8464ed8f77d185c3782597889025d8656a8f
Bright Kunda Mulomba <89812566+BSK-98@users.noreply.github.com>
2023-09-05 02:02:23 -0700

    Update index.js

--- Assets/scripts/utils/index.js
+++ Assets/scripts/utils/index.js
@@ -20,28 +20,35 @@ class Utils {
     }
 
     getCountDownTimer () {
-    let countdown =  new Date ("Sep 25, 2023 00:00:00").getTime ()
+        let countdown =  new Date ("October 25, 2023 00:00:00").getTime ()
 
-    let interval = setInterval (() => {
-        const day = 1000 * 60 * 60 * 24
-        const hour = 1000 * 60 * 60
-        const minuet = 1000 * 60
-        const second = 1000
+        let interval = setInterval (() => {
+            const day = 1000 * 60 * 60 * 24
+            const hour = 1000 * 60 * 60
+            const minuet = 1000 * 60
+            const second = 1000
 
-        let now = new Date ().getTime ()
+            let now = new Date ().getTime ()
 
-        let distance = countdown - now 
+            let distance = countdown - now 
 
-        let days = Math.floor (distance / day)
-        let hours = Math.floor (distance % day / hour)
-        let minuets = Math.floor (distance % hour / minuet)
+            let days = Math.floor (distance / day)
+            let hours = Math.floor (distance % day / hour)
+            let minuets = Math.floor (distance % hour / minuet)
 
-        let seconds = Math.floor (distance % minuet / second) 
+            let seconds = Math.floor (distance % minuet / second) 
 
             $ ('#days').html (days)
             $ ('#hours').html (hours)
             $ ('#minutes').html (minuets)
             $ ('#seconds').html (seconds)
+            if (distance > 0) {
+                clearInterval (interval)
+                days = '00'
+                hours = '00'
+                minuets = '00'
+                seconds = '00'
+            }
 
         }, 1000)
     }
